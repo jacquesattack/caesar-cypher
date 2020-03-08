b089191e4ddae04b067b0e70e9520d4c74867c18
jacquesattack <jacques.rossignol@gmail.com>
2020-03-07 20:23:45 -0800

    Choose random seed every time the program is run.

--- src/caesar_cypher/core.clj
+++ src/caesar_cypher/core.clj
@@ -1,6 +1,11 @@
 (ns caesar-cypher.core
   (:gen-class))
 
+(def inc-value (inc (rand-int 25)))
+
+(defn cypher-inc [value]
+  (+ inc-value value))
+
 (defn is-letter [letter]
   (let [int-letter (int letter)]
     (or 
@@ -11,10 +16,10 @@
   (not (is-letter letter)))
 
 (defn next-letter [letter]
-  (let [int-letter (inc (int letter))]
+  (let [int-letter (cypher-inc (int letter))]
     (cond
-      (> int-letter 122) (char 65)
-      (and (> int-letter 90) (< int-letter 97)) (char 97)
+      (> int-letter 122) (char (+ 65 (- int-letter 123)))
+      (and (> int-letter 90) (< int-letter 97)) (char (+ 97 (- int-letter 91)))
       :else (char int-letter))))
 
 (defn cypher-map [letter]
--- test/caesar_cypher/core_test.clj
+++ test/caesar_cypher/core_test.clj
@@ -11,15 +11,22 @@
     (is (= (is-letter \A) true))))
 
 (deftest next-letter-test
-  (testing "A"
-    (is (= (next-letter \A) \B)))
-  (testing "Z"
-    (is (= (next-letter \Z) \a)))
-  (testing "z"
-    (is (= (next-letter \z) \A))))
+  (with-redefs [inc-value 1]
+    (testing "A"
+      (is (= (next-letter \A) \B)))
+    (testing "Z"
+      (is (= (next-letter \Z) \a)))
+    (testing "z"
+      (is (= (next-letter \z) \A))))
+  (with-redefs [inc-value 2]
+    (testing "Z"
+      (is (= (next-letter \Z) \b)))
+    (testing "Z"
+      (is (= (next-letter \z) \B)))))
 
 (deftest cypher-test
-  (testing "cypher test"
-    (is (= (cypher "foo bar!") "gpp cbs!")))
-  (testing "cypher test"
-    (is (= (cypher "Zz") "aA"))))
+  (with-redefs [inc-value 1]
+    (testing "cypher test"
+      (is (= (cypher "foo bar!") "gpp cbs!")))
+    (testing "cypher test"
+      (is (= (cypher "Zz") "aA")))))
